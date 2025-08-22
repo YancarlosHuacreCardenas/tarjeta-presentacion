787b38eaa32c04abb10135f0ec5da066a7afbba0
Yancarlos Huacre Cardenas <yancarlos.huacre@vallegrande.edu.pe>
2025-08-22 09:21:35 -0500

    Subiendo proyecto Angular tarjeta-presentacion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,30 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [CommonModule],  // Para ngIf, ngFor y demás
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-
 export class AppComponent {
-  title = 'tarjeta-presentacion';
+  nombre: string = 'Yancarlos Huacre Cardenas';
+  carrera: string = 'Ingeniería en Sistemas';
+  edad: number = 19;
+  correo: string = 'yancarlos.huacre@vallegrande.edu.pe';
+  mostrarInfoExtra: boolean = false;
+  colorFondo: string = '#f9f9f9';
+
+  fraseFavorita: string = 'El conocimiento es poder.';
+  hobbies: string[] = ['Leer', 'Programar', 'Fútbol'];
+  skills: string[] = ['Angular', 'HTML', 'CSS', 'JavaScript'];
+
+  toggleExtra() {
+    this.mostrarInfoExtra = !this.mostrarInfoExtra;
+  }
+
+  cambiarColor() {
+    this.colorFondo = this.colorFondo === '#f9f9f9' ? '#e0f7fa' : '#f9f9f9';
+  }
 }
