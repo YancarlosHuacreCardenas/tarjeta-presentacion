c6fbd6a29de37d6c21e743748a3351f93263123b
ValeryChumpitaz <valery.chumpitaz@vallegrande.edu.pe>
2025-08-22 08:56:24 -0500

    Subiendo proyecto Angular tarjeta-presentacion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,30 +1,13 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule],  // Para ngIf, ngFor y demás
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  nombre: string = 'Yancarlos Huacre Cardenas';
-  carrera: string = 'Ingeniería en Sistemas';
-  edad: number = 19;
-  correo: string = 'yancarlos.huacre@vallegrande.edu.pe';
-  mostrarInfoExtra: boolean = false;
-  colorFondo: string = '#f9f9f9';
-
-  fraseFavorita: string = 'El conocimiento es poder.';
-  hobbies: string[] = ['Leer', 'Programar', 'Fútbol'];
-  skills: string[] = ['Angular', 'HTML', 'CSS', 'JavaScript'];
-
-  toggleExtra() {
-    this.mostrarInfoExtra = !this.mostrarInfoExtra;
-  }
-
-  cambiarColor() {
-    this.colorFondo = this.colorFondo === '#f9f9f9' ? '#e0f7fa' : '#f9f9f9';
-  }
+  title = 'tarjeta-presentacion';
 }
